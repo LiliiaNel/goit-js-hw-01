da8c7abb0c5bddaffdf3132da7002c5c08deb062
Lili <Liliia_Nel@proton.me>
2024-12-27 21:28:58 +0100

    add task 2 and task 3, linked js to html

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function getElementWidth(content, padding, border) {
+    const contentToNumber = Number.parseFloat(content);
+    const paddingToNumber = Number.parseFloat(padding);
+    const borderToNumber = Number.parseFloat(border);
+    return `${contentToNumber + ((paddingToNumber + borderToNumber) * 2)}`;
+    
+}
+getElementWidth("50px", "8px", "4px");
+getElementWidth("60px", "12px", "8.5px");
+getElementWidth("200px", "0px", "0px");
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
+
