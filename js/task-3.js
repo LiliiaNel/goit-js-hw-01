cacdd131b2cc6af9d6984a48d2b64029c369b772
Lili <Liliia_Nel@proton.me>
2024-12-28 18:50:07 +0100

    fix function to return nember

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ function getElementWidth(content, padding, border) {
     const contentToNumber = Number.parseFloat(content);
     const paddingToNumber = Number.parseFloat(padding);
     const borderToNumber = Number.parseFloat(border);
-    return `${contentToNumber + ((paddingToNumber + borderToNumber) * 2)}`;
+    return contentToNumber + ((paddingToNumber + borderToNumber) * 2);
     
 }
 getElementWidth("50px", "8px", "4px");
@@ -12,4 +12,5 @@ getElementWidth("200px", "0px", "0px");
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
+
 
